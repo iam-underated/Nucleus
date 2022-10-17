836b013f6ec659dcb849a334f4bb65138dde1965
mellobacon <mellodev@outlook.com>
2022-10-17 17:27:41 -0500

    rust is so confusing

--- src-tauri/src/directory.rs
+++ src-tauri/src/directory.rs
@@ -1,12 +1,14 @@
-use notify::{RecursiveMode, Watcher, RecommendedWatcher, Config};
+use async_std::task::{self};
+use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
 use std::{sync::mpsc::channel};
-use async_std::task::{self, JoinHandle};
 
-pub fn directory_watcher(dir: &'static str) -> JoinHandle<()> {
-    let t = task::spawn(async move{
+pub fn directory_watcher(dir: &'static str) {
+    task::spawn(async move {
         let (sender, receiver) = channel();
-        let mut watcher:RecommendedWatcher = Watcher::new(sender, Config::default()).unwrap();
-        watcher.watch(dir.as_ref(), RecursiveMode::Recursive).unwrap();
+        let mut watcher: RecommendedWatcher = Watcher::new(sender, Config::default()).unwrap();
+        watcher
+            .watch(dir.as_ref(), RecursiveMode::Recursive)
+            .unwrap();
         loop {
             match receiver.recv() {
                 Ok(event) => {
@@ -16,7 +18,6 @@ pub fn directory_watcher(dir: &'static str) -> JoinHandle<()> {
             }
         }
     });
-    return t
 }
 
 
